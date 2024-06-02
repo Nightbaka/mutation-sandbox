c87949fa0b8ae28383e17372a4b880b4dc8c2213
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2024-06-02 18:47:08 +0200

    shit

--- users/signals.py
+++ users/signals.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 @receiver(user_signed_up)
 def populate_user(sender, request, user, **kwargs):
+    print("populate user")
     sociallogin = kwargs.get('sociallogin')
     if sociallogin:
+        print("Social login")
         if sociallogin.account.provider == 'google':
+            print("google passed")
             extra_data = sociallogin.account.extra_data
             user.first_name = extra_data.get('given_name', '')
             user.last_name = extra_data.get('family_name', '')
