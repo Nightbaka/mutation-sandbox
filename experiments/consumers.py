ac17a26b4c2e978a0f0e5286eb3ff5dc753e783a
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2024-06-21 16:44:47 +0200

    Run history backend impl

--- experiments/consumers.py
+++ experiments/consumers.py
@@ -2,16 +2,45 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.serializers import serialize
 import django
+from django.db.models import Count, Max, Avg
+
 django.setup()
-from .models import Experiment
+from .models import Experiment, Population, Individual
 from .serializers import ExperimentSerializer
 from django.db import connection, models
 from asgiref.sync import sync_to_async
 
 @sync_to_async
 def get_experiments():
-    exp = Experiment.objects.all()
-    srl = serialize("json", exp)
+    experiments = Experiment.objects.annotate(
+        num_pops = Count('population', distinct=True),
+        best_fitness = Max('population__individual__fitness'),
+        avg_fitness = Avg('population__individual__fitness')
+    )
+
+    exp_payload = []
+
+    for exp in experiments:
+        serialized_exp = {
+            'id': exp.id,
+            'num_pops': exp.num_pops,
+            'best_fitness': exp.best_fitness,
+            'avg_fitness': exp.avg_fitness
+        }
+        exp_payload.append(serialized_exp)
+
+    return json.dumps(exp_payload)
+
+@sync_to_async
+def get_populations():
+    pops = Population.objects.all()
+    srl = serialize("json", pops)
+    return srl
+
+@sync_to_async
+def get_individuals():
+    pops = Population.objects.all()
+    srl = serialize("json", pops)
     return srl
 
 
@@ -33,7 +62,7 @@ class MyConsumer(AsyncWebsocketConsumer):
         }))
 
 
-class ExperimentConsumer(AsyncWebsocketConsumer):
+class HistoryConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
--- experiments/routing.py
+++ experiments/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from experiments import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/experiment/$', consumers.ExperimentConsumer.as_asgi()),
+    # re_path(r'ws/experiment/$', consumers.ExperimentConsumer.as_asgi()),
+    re_path(r'ws/history/$', consumers.HistoryConsumer.as_asgi())
 ]
